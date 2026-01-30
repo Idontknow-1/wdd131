5bd4ec5ae476f7292aa9452b7b5520e4025d6271
idontknow-1 <imera@byupathway.edu>
2026-01-29 18:22:04 -0700

    finished place

--- scripts/place.js
+++ scripts/place.js
@@ -1,3 +1,26 @@
+// static weather
+const temperature = 26;
+const windSpeed = 13;
+
+// windchill cal
+function calculateWindChill (temp, speed) {
+    return (
+        13.12 + .6215 * temp - 11.37 * Math.pow(speed, .16) + .3965 *
+        temp * Math.pow(speed, .16)
+    ).toFixed(1);
+}
+
+const windChillSpan = document.querySelector("#windchill");
+
+// if valid and else
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChillSpan.textContent = `${calculateWindChill(temperature, windSpeed)} °C`;
+}
+else {
+    windChillSpan.textContent = "N/A"
+}
+
+// footer js
 const year = document.querySelector("#currentyear");
 year.textContent = new Date().getFullYear();
 

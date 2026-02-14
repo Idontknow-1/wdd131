bdf0f7b38b45c40094dd5a2d2af4f288df0cf93b
idontknow-1 <imera@byupathway.edu>
2026-02-14 03:01:04 -0700

    updates

--- project/scripts/lamerameranails.js
+++ project/scripts/lamerameranails.js
@@ -26,3 +26,40 @@ const nailForm = document.getElementById('nail-form');
         nailForm.style.display = 'none';
         successMsg.style.display = 'block';
     });
+
+// sizing options
+const sizing = [
+  { id: "method1", name: "Measuring Tape"},
+  { id: "method2", name: "Coin"},
+  { id: "method3", name: "Sizing Kit"}
+];
+
+const sizingSelect = document.getElementById("sizing");
+
+if (sizingSelect) {
+  sizing.forEach(sizing => {
+    const option = document.createElement("option");
+    option.value = sizing.id;
+    option.textContent = sizing.name;
+    sizingSelect.appendChild(option);
+  });
+}
+
+// tiers options
+const tiers = [
+  { id: "tier1", name: "Allium Tier"},
+  { id: "tier2", name: "Lavender Tier"},
+  { id: "tier3", name: "Time Circuits"},
+  { id: "tier4", name: "Low Voltage Reactor"}
+];
+
+const tiersSelect = document.getElementById("tiers");
+
+if (tiersSelect) {
+  tiers.forEach(tiers => {
+    const option = document.createElement("option");
+    option.value = tiers.id;
+    option.textContent = tiers.name;
+    tiersSelect.appendChild(option);
+  });
+}
